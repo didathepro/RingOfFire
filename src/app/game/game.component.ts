9094808d51467b37ca9c43ec32e3cab0a2148757
didathepro <didovicgraz@gmail.com>
2025-02-13 11:46:49 +0100

    showing card from stack and shuffle function

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss'] 
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation: boolean = false;
-  takeCard(){
-    this.pickCardAnimation = true;
+  currentCard: string = '';
+  game!: Game;
+
+  ngOnInit(): void {
+    this.newGame();  
   }
+
+  newGame(): void {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard(): void {
+    if(!this.pickCardAnimation){
+    this.currentCard = this.game.stack.pop() as string;
+    this.game.playedCards.push(this.currentCard);
+    this.pickCardAnimation = true;
+
+    setTimeout(() => {
+      this.pickCardAnimation = false;
+    },1500)
+  }
+}
 }
